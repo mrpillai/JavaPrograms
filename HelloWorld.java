ccb0b039c877d47bf69f6f88c6f6338cbefcd798
mrpillai <31706271+mrpillai@users.noreply.github.com>
2019-12-02 05:46:28 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author mrpillai
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Java Program");
+		
+	}
+
+}
